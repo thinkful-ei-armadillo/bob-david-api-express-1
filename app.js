cb788c39a8ed4ed365c60c0f49db19373e642462
bobnearents <bobnearents@yahoo.com>
2019-02-19 09:12:51 -0800

    finished cipher

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
 
 const app = express();
 app.use(morgan('dev'));
@@ -7,14 +7,24 @@ app.use(morgan('dev'));
 app.get('/sum', (req, res) => {
   // const a = req.query.a;
   // const b = req.query.b;
-
   const { a, b } = req.query;
-
-
-
 
   res.send(`The sum of ${a} and ${b} is ${parseInt(a) + parseInt(b)}`)
 })
+
+app.get('/cipher', (req,res) => {
+
+  const { text, shift } = req.query;
+
+  let results = ''
+
+  for (let i=0; i<text.length; i++) {
+    results += String.fromCharCode(text.charCodeAt(i) + parseInt(shift));
+  }
+  res.send(results);
+})
+
+
 
 app.listen(8000, () => {
   console.log('Express server is listening on server 8000')
